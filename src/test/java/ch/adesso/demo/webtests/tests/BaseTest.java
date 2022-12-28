8c58b44bd4a37f90ab7155546be80e550b6216af
KOELN\A17877C <jan.largin@rewe-group.com>
2022-12-28 17:38:37 +0100

    Cookies rejection fix

--- src/test/java/ch/adesso/demo/webtests/tests/BaseTest.java
+++ src/test/java/ch/adesso/demo/webtests/tests/BaseTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 
 import ch.adesso.demo.webtests.config.WebDriverProvider;
@@ -20,19 +21,22 @@ import io.qameta.allure.selenide.AllureSelenide;
 public class BaseTest {
 
     @BeforeAll
-    public static void beforeAll() {
+    public static void webDriverProviderConfig() {
         WebDriverProvider.config();
     }
 
-    @BeforeAll
-    public static void acceptCookies() {
-        open("/");
-        $("#ppms_cm_reject-all").click();
+    @BeforeEach
+    public void selenideLogger() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @BeforeEach
-    void beforeEach() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    public void rejectCookies() {
+        open("/");
+        SelenideElement rejectButton = $("#ppms_cm_reject-all");
+        if (rejectButton.exists()) {
+            rejectButton.click();
+        }
     }
 
     @AfterEach
